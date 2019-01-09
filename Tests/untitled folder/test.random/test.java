7b85305c2ad6ed7de375df6f281617d9d19c4d85
Jeremiah <6wa9jeremiah@users.noreply.github.com>
2019-01-09 10:43:50 -0800

    random

---
+++ Tests/untitled folder/test.random/test.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+/**
+ * Write a description of class test here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class test
+{
+    public static void main(){
+        
+    }
+}
